87c4bd8a4a798525b87fdce5ca6cee78b1b1af76
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2020-08-19 09:48:12 +0200

    Lint files

--- pyvrt/traffic.py
+++ pyvrt/traffic.py
@@ -2,22 +2,18 @@ from .http import vrt_api_request
 
 
 def traffic_teaser():
-    res = vrt_api_request('traffic', 'teaser', params={
-      'accept': 'application/vnd.traffic.vrt.be.teaser_1.0+json'
-    })
+    res = vrt_api_request(
+        "traffic",
+        "teaser",
+        params={"accept": "application/vnd.traffic.vrt.be.teaser_1.0+json"},
+    )
     return res
 
 
 def traffic_jam(km=False):
     traffic_data = traffic_teaser()
-    traffic_jam_length = traffic_data['teaser']['trafficJamLength']
+    traffic_jam_length = traffic_data["teaser"]["trafficJamLength"]
     if km:
-        return {
-          'length': traffic_jam_length / 1000,
-          'unit': 'km'
-        }
+        return {"length": traffic_jam_length / 1000, "unit": "km"}
     else:
-        return {
-          'length': traffic_jam_length,
-          'unit': 'm'
-        }
+        return {"length": traffic_jam_length, "unit": "m"}
--- pyvrt/weather.py
+++ pyvrt/weather.py
@@ -3,94 +3,85 @@ from .http import vrt_api_request
 
 def __get_zone_from_list(items_list, zone):
     try:
-        res = filter(
-            lambda item: item['location'].lower() == zone.lower(),
-            items_list
-        )
+        res = filter(lambda item: item["location"].lower() == zone.lower(), items_list)
         return res
     except (KeyError, StopIteration) as ex:
-        raise ValueError('Could not find zone in list', ex, items_list)
+        raise ValueError("Could not find zone in list", ex, items_list)
 
 
-def summary(region='BE'):
-    if region is not 'BE':
-        raise ValueError("Only region == 'BE' is supported for retrieving summary")
+def summary(region="BE"):
+    if region != "BE":
+        raise ValueError("Only region=BE is supported for retrieving summary")
 
-    res = vrt_api_request('weather', 'weathertalks/belgie_huidig', params={
-        'accept': 'application/vnd.weather.vrt.be.weathertalks_1.0+json'
-    })
+    res = vrt_api_request(
+        "weather",
+        "weathertalks/belgie_huidig",
+        params={"accept": "application/vnd.weather.vrt.be.weathertalks_1.0+json"},
+    )
 
     if res:
-        return res['weathertalk']
+        return res["weathertalk"]
     else:
         return None
 
 
-def current(region='BE', zone=None):
-    if region is 'BE':
+def current(region="BE", zone=None):
+    if region is "BE":
         res = vrt_api_request(
-            'weather',
-            'observations/belgische_streken',
-            params={
-              'accept': 'application/vnd.weather.vrt.be.observations_1.0+json'
-            }
+            "weather",
+            "observations/belgische_streken",
+            params={"accept": "application/vnd.weather.vrt.be.observations_1.0+json"},
         )
 
     if zone:
-        zone_res = next(__get_zone_from_list(res['observations'], zone))
-        zone_res['date'] = res['date']
+        zone_res = next(__get_zone_from_list(res["observations"], zone))
+        zone_res["date"] = res["date"]
         return zone_res
 
     return res
 
-    if region is 'EU':
+    if region is "EU":
         res = vrt_api_request(
-            'weather',
-            'observations/europese_steden',
-            params={
-              'accept': 'application/vnd.weather.vrt.be.observations_1.0+json'
-            }
+            "weather",
+            "observations/europese_steden",
+            params={"accept": "application/vnd.weather.vrt.be.observations_1.0+json"},
         )
 
     if zone:
-        zone_res = next(__get_zone_from_list(res['observations'], zone))
-        zone_res['date'] = res['date']
+        zone_res = next(__get_zone_from_list(res["observations"], zone))
+        zone_res["date"] = res["date"]
         return zone_res
     else:
         return res
 
 
-def forecast(region='BE', zone=None):
-    if region is 'BE':
+def forecast(region="BE", zone=None):
+    if region is "BE":
         res = vrt_api_request(
-            'weather',
-            'forecasts/belgische_streken',
-            params={
-              'accept': 'application/vnd.weather.vrt.be.forecasts_1.0+json'
-            }
+            "weather",
+            "forecasts/belgische_streken",
+            params={"accept": "application/vnd.weather.vrt.be.forecasts_1.0+json"},
         )
 
         if zone:
             return {
-              'date': res['date'],
-              'forecasts': list(__get_zone_from_list(res['forecasts'], zone)),
+                "date": res["date"],
+                "forecasts": list(__get_zone_from_list(res["forecasts"], zone)),
             }
         else:
             return res
 
-    if region is 'EU':
+    if region is "EU":
         res = vrt_api_request(
-            'weather',
-            'forecasts/europese_steden',
-            params={
-              'accept': 'application/vnd.weather.vrt.be.forecasts_1.0+json'
-            }
+            "weather",
+            "forecasts/europese_steden",
+            params={"accept": "application/vnd.weather.vrt.be.forecasts_1.0+json"},
         )
 
         if zone:
             return {
-              'date': res['date'],
-              'forecasts': list(__get_zone_from_list(res['forecasts'], zone)),
+                "date": res["date"],
+                "forecasts": list(__get_zone_from_list(res["forecasts"], zone)),
             }
         else:
             return res
