418ff071a6e4b61fc88affe9ba76e1a8bf44554f
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2021-02-01 18:09:38 +0100

    Add metadata for 1.7.0

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# 1.7.0
+
+- Adds the possibility to interact with media services. Limited to StuBru radio right now
--- pyvrt/media/stubru.py
+++ pyvrt/media/stubru.py
@@ -1,6 +1,7 @@
 from ..http import vrt_api_request
 from .const import CHANNEL_CODE_STUBRU
 
+
 def __get_image_url(image_urls):
     try:
         res = filter(lambda item: item["destinations"] == "radioplus", image_urls)
@@ -8,65 +9,68 @@ def __get_image_url(image_urls):
     except (KeyError, StopIteration) as ex:
         raise ValueError("Could not find a suitable image", ex, image_urls)
 
+
 def get_onair():
     onairs = vrt_api_request(
         "epg",
         "onair",
         params={
-          "accept": "application/vnd.epg.vrt.be.onairs_1.2+json",
-          "channel_code": str(CHANNEL_CODE_STUBRU),
+            "accept": "application/vnd.epg.vrt.be.onairs_1.2+json",
+            "channel_code": str(CHANNEL_CODE_STUBRU),
         },
     )
     if onairs and onairs.get("onairs"):
-      return onairs["onairs"][0]
+        return onairs["onairs"][0]
     else:
-      return {}
+        return {}
 
 
 def previous():
     onair = get_onair()
 
     if onair.get("previous"):
-      item = onair["previous"]
-      imageItem = next(__get_image_url(item["imageurls"]))
-      return {
-        "title": item["title"],
-        "description": item["description"],
-        "startTime": item["startTime"],
-        "endTime": item["endTime"],
-        "image": imageItem["url"] if imageItem else None
-      }
+        item = onair["previous"]
+        imageItem = next(__get_image_url(item["imageurls"]))
+        return {
+            "title": item["title"],
+            "description": item["description"],
+            "startTime": item["startTime"],
+            "endTime": item["endTime"],
+            "image": imageItem["url"] if imageItem else None,
+        }
     else:
-      return {}
+        return {}
+
 
 def next_up():
     onair = get_onair()
 
     if onair.get("next"):
-      item = onair["next"]
-      imageItem = next(__get_image_url(item["imageurls"]))
-      return {
-        "title": item["title"],
-        "description": item["description"],
-        "startTime": item["startTime"],
-        "endTime": item["endTime"],
-        "image": imageItem["url"] if imageItem else None
-      }
+        item = onair["next"]
+        imageItem = next(__get_image_url(item["imageurls"]))
+        return {
+            "title": item["title"],
+            "description": item["description"],
+            "startTime": item["startTime"],
+            "endTime": item["endTime"],
+            "image": imageItem["url"] if imageItem else None,
+        }
     else:
-      return {}
+        return {}
+
 
 def current():
     onair = get_onair()
 
     if onair.get("now"):
-      item = onair["now"]
-      imageItem = next(__get_image_url(item["imageurls"]))
-      return {
-        "title": item["title"],
-        "description": item["description"],
-        "startTime": item["startTime"],
-        "endTime": item["endTime"],
-        "image": imageItem["url"] if imageItem else None
-      }
+        item = onair["now"]
+        imageItem = next(__get_image_url(item["imageurls"]))
+        return {
+            "title": item["title"],
+            "description": item["description"],
+            "startTime": item["startTime"],
+            "endTime": item["endTime"],
+            "image": imageItem["url"] if imageItem else None,
+        }
     else:
-      return {}
+        return {}
